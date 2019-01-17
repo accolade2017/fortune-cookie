bb737cfde3ce75ed020b1a5b7493f699c18ef5e5
k.sato <cowkey.gem@gmail.com>
2019-01-17 19:56:41 +0900

    とりあえずガワだけ

---
+++ app/index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css" />
+</head>
+<body>
+  
+  
+</body>
+</html>
+
+
+
+
