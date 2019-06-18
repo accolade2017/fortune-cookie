c8488ac8c72e0c92282799e83618860d7ddfe9e7
k.sato <cowkey.gem@gmail.com>
2019-06-19 06:31:10 +0900

    DDL自体を追加

---
+++ db/DDL.sql
@@ -0,0 +1,26 @@
+create table users_fortunes (
+ user_id BIGINT not null
+ , fortune_ymd VARCHAR(8) not null
+ , fortune_id BIGINT not null
+ , constraint users_fortunes_PKC primary key (user_id,fortune_ymd)
+) ;
+
+create table fortunes (
+ fortune_id BIGINT not null
+ , message TEXT
+ , path VARCHAR(500) not null
+ , score INT not null
+ , enabled BINARY default 1 not null
+ , constraint fortunes_PKC primary key (fortune_id)
+) ;
+
+create table users (
+ user_id BIGINT auto_increment not null
+ , login_id VARCHAR(50) not null
+ , password VARCHAR(255) not null
+ , user_type CHAR(1) default '1' not null
+ , created_at DATETIME not null
+ , constraint users_PKC primary key (user_id)
+) ;
+
+alter table users add unique users_login_id_uniq (login_id) ;
