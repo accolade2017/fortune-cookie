fbca24f28d70f9e9110884b8d827a3ba11bd56ce
k.sato <cowkey.gem@gmail.com>
2019-09-27 00:06:39 +0900

    DB設定を本番向きに変更する

--- db/DDL.sql
+++ db/DDL.sql
@@ -10,17 +10,18 @@ create table fortunes (
  , message TEXT
  , path VARCHAR(500) not null
  , score INT not null
- , enabled BINARY default 1 not null
+ , enabled smallint default 1 not null
  , constraint fortunes_PKC primary key (fortune_id)
 ) ;
 
 create table users (
- user_id BIGINT auto_increment not null
+ user_id serial not null
  , login_id VARCHAR(50) not null
  , password VARCHAR(255) not null
  , user_type CHAR(1) default '1' not null
- , created_at DATETIME not null
+ , created_at timestamp not null
  , constraint users_PKC primary key (user_id)
 ) ;
 
 alter table users add unique users_login_id_uniq (login_id) ;
+alter table users add column slack_user_name varchar(255);
